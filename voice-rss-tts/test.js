107dbec4d1ead5f9f92fadaf18f80c1c14be1de5
Michael Baska <mikebaska1@gmail.com>
2020-03-22 17:17:39 -0500

    TTS implementation

---
+++ voice-rss-tts/test.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var tts = require('./index.js');
+
+var appt = 'Showtime will be in ';
+var cntdwn = '5 minutes.';
+
+http.createServer(function (request, response) {
+  tts.speech({
+    key: '7c94d8a1aad74328bec5315298f927c2',
+    hl: 'en-us',
+    src: appt + cntdwn,
+    r: -1,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+    b64: false,
+    callback: function (error, content) {
+      response.end(error || content);
+    }
+  });
+}).listen(8081);
