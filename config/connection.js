878c315eca2406cc691bfb63ccec0daeef8fdfb7
Alek <56727628+aseawright1@users.noreply.github.com>
2020-03-25 20:17:47 -0500

    deleted all non-recurring code. reorganizing filesystem (incomplete)

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("scheduler", "root", "thedagon26", {
+var sequelize = new Sequelize("scheduler", "root", "password", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
@@ -18,7 +18,7 @@ var mysqlconfig = {
 	"connectionLimit": 10,
 	"host": "localhost",
 	"user": "root",
-	"password": "thedagon26",
+	"password": "password",
 	"database": "scheduler"
 };
 
--- seeds/schema.sql
+++ seeds/schema.sql
@@ -1,17 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `scheduler_howto_node`;
-USE `scheduler_howto_node`;
+CREATE DATABASE IF NOT EXISTS `scheduler`;
+USE `scheduler`;
 
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE `events` (
-  `id` bigint(20) unsigned AUTO_INCREMENT,
-  `start_date` datetime NOT NULL,
-  `end_date` datetime NOT NULL,
-  `text` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `recurring_events`;
-CREATE TABLE `recurring_events` (
   `id` bigint(20) unsigned AUTO_INCREMENT,
   `start_date` datetime NOT NULL,
   `end_date` datetime NOT NULL,
