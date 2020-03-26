49894dd044065e39a08aef2c7dbef232b0d24b79
jared Higgins <hjared10@yahoo.com>
2020-03-25 18:53:30 -0600

    rewrote for sequelize

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+const routes = require("./routes/html-routes.js");
+const mysqlconfig = require("./config/connection.js");
 
-var express = require("express");
+const util = require("util");
+const mysql = require('mysql');
 
-var app = express();
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
+const helmet = require("helmet");
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// you'll need these headers if your API is deployed on a different domain than a public page
+// in production system you could set Access-Control-Allow-Origin to your domains
+// or drop this expression - by default CORS security is turned on in browsers
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "*");
+	next();
+});
 
-app.use(express.static("public"));
+// return static pages from "./public" directory
+app.use(express.static(__dirname + "/public"));
 
 
-require("./routes/html_routes.js")(app);
 
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// open connection to mysql
+const connectionPool = mysql.createPool(mysqlconfig);
+connectionPool.query = util.promisify(connectionPool.query);
+
+// add listeners to basic CRUD requests
+const Storage = require("./models/storage.js");
+const eventsStorage = new Storage(connectionPool);
+routes.setRoutes(app, "/events", eventsStorage);
+
+// add listeners to basic CRUD with recurring events support
+const RecurringStorage = require("./models/storage_recurring");
+const recurringEventsStorage = new RecurringStorage(connectionPool);
+routes.setRoutes(app, "/recurring_events", recurringEventsStorage)
+
+// start server
+app.listen(port, () => {
+	console.log("Server is running on port " + port + "...");
 });
