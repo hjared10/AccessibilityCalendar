0f351d9d5f61a47f0865cc3c98c73f56da6654ec
Alek <56727628+aseawright1@users.noreply.github.com>
2020-03-25 20:53:13 -0500

    include jawDB in connection.js

--- config/connection.js
+++ config/connection.js
@@ -19,7 +19,9 @@ var mysqlconfig = {
 	"host": "j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 	"user": "	p8xo2kpzw1t8y5tb",
 	"password": "eb6iqsopc43lzi4m",
-	"database": "fp0ek73mpnh14exz"
+  "database": "fp0ek73mpnh14exz",
+  "use_env_variable": "JAWSDB_URL",
+  "dialect": "mysql"
 };
 
 module.exports = mysqlconfig;
